c011f6b9868f3b97bd57e4dc0034082059faef3d
David Hindman <github@hnd.mn>
2024-03-07 15:41:17 -0500

    Added basic layout and nav for mvp routes

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let title;
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">{title}</h1>
+	</div>
+</div>
---
+++ src/lib/stores/farmer.js
@@ -0,0 +1,4 @@
+import { writable } from 'svelte/store'
+export const established = writable('')
+export const name = writable('')
+export const farmName = writable('')
