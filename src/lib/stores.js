14aea514455a0eac966182479488f43a9e6422cb
David Hindman <github@hnd.mn>
2024-03-12 17:01:08 -0400

    initial setup complete

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -7,6 +7,10 @@ export const farmerId = localStorageStore(`${prefix}-farmer-id`, '');
 export const farmerName = localStorageStore(`${prefix}-farmer-name`, '');
 export const farmName = localStorageStore(`${prefix}-farm-name`, '');
 
+// Harvest stores
+export const harvests = localStorageStore(`${prefix}-harvests`, []);
+export const traySizes = localStorageStore(`${prefix}-tray-sizes`, ['1020', '520', 'mini']);
+
 // Supplies stores
 export const supplies = localStorageStore(`${prefix}-supplies`, []);
 export const supplyTypes = localStorageStore(`${prefix}-supplies`, ['seeds', 'soil', 'trays']);
--- src/routes/harvests/+page.svelte
+++ src/routes/harvests/+page.svelte
@@ -1,1 +1,51 @@
-[Table goes here]
+<script>
+	// @ts-nocheck
+	import { getModalStore } from '@skeletonlabs/skeleton';
+	import { harvests } from '$lib/stores';
+	import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
+	export let data;
+
+	$harvests = data.harvests.map((harvest) => ({
+		...harvest,
+		harvestedAt: new Date(harvest.harvestedAt).toLocaleDateString('en-US', {
+			month: 'long',
+			day: 'numeric',
+			year: 'numeric'
+		}),
+		grams: harvest.quantity.toLocaleString('en-US')
+	}));
+
+	$: datatable = {
+		// A list of heading labels.
+		head: ['Date', 'Crop', 'Tray', 'Grams'],
+		// The data visibly shown in your table body UI.
+		body: tableMapperValues($harvests, ['harvestedAt', 'crop', 'traySize', 'grams']),
+		// Optional: The data returned when interactive is enabled and a row is clicked.
+		meta: tableMapperValues(data.harvests, ['id', 'harvestedAt', 'crop', 'traySize', 'grams']),
+		// Optional: A list of footer labels.
+		foot: ['', '', '', '']
+	};
+
+	const modalStore = getModalStore();
+
+	function handleSelection(event) {
+		console.log(event.detail);
+		const selectedHarvest = event.detail.reduce((obj, value, index) => {
+			const key = ['id', 'harvestedAt', 'crop', 'traySize', 'grams'][index];
+			obj[key] = value;
+			return obj;
+		}, {});
+		const modalUpdate = {
+			type: 'component',
+			component: 'harvestsModal',
+			meta: {
+				action: 'update',
+				supply: selectedHarvest
+			}
+		};
+
+		modalStore.trigger(modalUpdate);
+	}
+</script>
+
+<Table source={datatable} interactive={true} on:selected={handleSelection} />
