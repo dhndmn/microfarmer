b52d4444222dc91a49e9300502827b57f25a175a
David Hindman <github@hnd.mn>
2024-03-14 11:52:36 -0400

    Mock dashboard components added

---
+++ src/lib/components/farm/HarvestSummary.svelte
@@ -0,0 +1,30 @@
+<script>
+	// @ts-nocheck
+	export let harvestGrams;
+
+	function animalMath(harvestGrams) {
+		const animals = [
+			{ gerbil: { grams: 100 } },
+			{ gopher: { grams: 400 } },
+			{ snail: { grams: 25 } },
+			{ squirrel: { grams: 250 } },
+			{ wombat: { grams: 75 } }
+		];
+		const randomAnimalIndex = Math.floor(Math.random() * animals.length);
+		const animal = Object.keys(animals[randomAnimalIndex])[0];
+		const animalGrams = animals[randomAnimalIndex][animal].grams;
+		const equivalentAnimals = (harvestGrams / animalGrams).toFixed(1);
+		return `or ${equivalentAnimals} ${animal}s`;
+	}
+</script>
+
+<div class="p-4 card">
+	<section class="flex flex-col items-center justify-center p-4">
+		<p class="text-5xl font-semibold">
+			{Math.floor(harvestGrams / 1000)}
+		</p>
+		<p>kilograms</p>
+		<p>harvested</p>
+		<p class="my-2 text-sm italic">({animalMath(harvestGrams)})</p>
+	</section>
+</div>
--- src/routes/farm/+page.svelte
+++ src/routes/farm/+page.svelte
@@ -1,5 +1,8 @@
 <script>
-	import { farmerName } from '$lib/stores';
+	import { farmerName, harvests, supplies } from '$lib/stores';
+	import CostPer from '$lib/components/farm/CostPer.svelte';
+	import HarvestSummary from '$lib/components/farm/HarvestSummary.svelte';
+	import TopCrops from '$lib/components/farm/TopCrops.svelte';
 	const getDayTime = () => {
 		const hour = new Date().getHours();
 		if (hour < 12) return 'morning';
@@ -7,8 +10,14 @@
 		return 'evening';
 	};
 	const dayTime = getDayTime();
+	const harvestGrams = $harvests.reduce((total, harvest) => total + harvest.grams, 0);
+	const totalSpending = $supplies.reduce((total, supply) => total + supply.price, 0);
 </script>
 
 <p>Good {dayTime}, farmer {$farmerName}!</p>
 
-[Dashboard goes here]
+<div class="grid grid-cols-1 gap-4 my-12">
+	<CostPer harvestGrams totalSpending />
+	<HarvestSummary harvestGrams />
+	<TopCrops />
+</div>
